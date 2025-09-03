4bddd3f74a6ddff862283e8f8f502f854e4aaa49
Rohit <rohitnarnolia88@gmail.com>
2025-09-03 10:22:48 +0530

    Time: 0 ms (100%), Space: 19.4 MB (83.98%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,63 +1,53 @@
 
 class Solution {
 public:
-    vector<int>size;
-    vector<int>parent;
+    vector<int> size;
+    vector<int> parent;
 
-    int find(int x){
-        return parent[x]=(parent[x]==x)?x:find(parent[x]);
+    int find(int x) {
+        return parent[x] = (parent[x] == x) ? x : find(parent[x]);
     }
 
-    void union_dsu(int a,int b){
-        a=find(a);
-        b=find(b);
+    void union_dsu(int a, int b) {
+        a = find(a);
+        b = find(b);
 
-        if(a==b) return;
+        if (a == b)
+            return;
 
-        //otherwise check the sizes
-        if(size[a]>=size[b]){
-            parent[b]=a;
-            size[a]+=size[b];
-            size[b]=0;
-        }
-        else{
-            parent[a]=b;
-            size[b]+=size[a];
-            size[a]=0;
+        // otherwise check the sizes
+        if (size[a] >= size[b]) {
+            parent[b] = a;
+            size[a] += size[b];
+            size[b] = 0;
+        } else {
+            parent[a] = b;
+            size[b] += size[a];
+            size[a] = 0;
         }
     }
 
-   
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int ans = 0;
         int n = isConnected.size();
         int m = isConnected[0].size();
 
         parent.resize(n);
-        for(int i=0;i<n;i++){
-            parent[i]=i;
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
         }
-        size.resize(n,1);
-
-        //create an edgelist
-        vector<pair<int,int>>edgeList;
-
+        size.resize(n, 1);
 
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (isConnected[i][j] == 1) {
-                    edgeList.push_back({i,j});
-                }
+            for (int j = 0; j < n; j++) { 
+                if (isConnected[i][j] == 1)
+                    union_dsu(i, j);
             }
         }
 
-        //after creating this edge list I can apply dsu
-        for(auto pp:edgeList){
-            union_dsu(pp.first,pp.second);
-        }
-        
-        for(auto s:size){
-            if(s>0) ans++;
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            if (find(i) == i)
+                ans++;
         }
         return ans;
     }
