621d4454e84743961f0c00c3ceeacc776dc93812
Rohit <rohitnarnolia88@gmail.com>
2025-09-04 16:18:46 +0530

    Time: 3 ms (31.66%), Space: 10 MB (42.12%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -3,7 +3,8 @@ public:
     vector<vector<int>> subsets(vector<int>& nums) {
         int subsetSize= 1<<nums.size();
         int n = nums.size();
-
+        //T.C -> (2^n)*n
+        //S.C -> (2^n) -> if all subset are of length n then *n too
         vector<vector<int>>ans;
         for(int i=0;i<subsetSize;i++){
             vector<int>v;
