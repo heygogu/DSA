22450ffb5823362fd517cd26453835af28e5f3b5
Rohit <rohitnarnolia88@gmail.com>
2025-09-07 07:53:34 +0530

    Time: 0 ms (100%), Space: 9.7 MB (53.26%) - LeetHub

---
+++ 1426-find-n-unique-integers-sum-up-to-zero/1426-find-n-unique-integers-sum-up-to-zero.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> sumZero(int n) {
+        if(n==1) return {0};
+        if(n==2) return {-1,1};
+
+        int left = n/2*-1;
+        int right = n/2;
+        bool isEven=n%2==0;
+
+        vector<int>ans;
+        for(int i=left;i<=right;i++){
+            if(isEven && i==0)continue; 
+            ans.push_back(i);
+        }
+
+        return ans;
+    }
+};
