1cf215f19d4bf3fc66c66d2d4b8ae60b01a73281
Rohit <rohitnarnolia88@gmail.com>
2025-09-09 09:14:56 +0530

    Time: 21 ms (14.11%), Space: 8.1 MB (92.33%) - LeetHub

--- 2408-number-of-people-aware-of-a-secret/2408-number-of-people-aware-of-a-secret.cpp
+++ 2408-number-of-people-aware-of-a-secret/2408-number-of-people-aware-of-a-secret.cpp
@@ -4,16 +4,14 @@ public:
     int dp[1001];
 
     int solve(int idx,int d,int f,int n){
-        if(idx+d>n) return 1;
+        if(idx>=n) return 1;
 
         if(dp[idx]!=-1) return dp[idx];
 
         int ans=1;
         for(int i=idx+d;i<=min(idx+f,n);i++){
             if(i==idx+f) {ans--; break;}
-
             ans=(ans%mod + solve(i,d,f,n)%mod)%mod;
-
         } 
 
         return dp[idx]=ans;
