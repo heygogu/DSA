d7cd4697ad2f1f249797cfd71da4fa4243e284e3
Rohit <rohitnarnolia88@gmail.com>
2025-09-12 07:39:17 +0530

    Time: 14 ms (21.08%), Space: 19.2 MB (16.23%) - LeetHub

---
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool doesAliceWin(string s) {
+        int count=0;
+        string vowels = "aeiou";
+        for(auto& ch: s){
+            if(vowels.find(ch)!=string::npos){
+                count++;
+            }
+        }
+
+        if(count>0) return true;
+        return false;
+    }
+};
